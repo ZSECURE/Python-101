7cb95288dbd94d5993101203e9e42356e33b578d
Zak Clifford <46941177+ZSECURE@users.noreply.github.com>
2022-08-21 22:16:50 +0100

    Update ssh-bruteforce.py

--- ssh-bruteforce.py
+++ ssh-bruteforce.py
@@ -10,6 +10,7 @@ if len(sys.argv) != 4:
 	print("\n")
 	exit()
 
+# username = ["root", "test", "admin", "oracle", "user", "nagios", "guest", "postgres", "info", "mysql", "ubuntu", "kali", "adm", "administrator", "apache", "at", "backup", "bb", "bin", "cron", "daemon", "db2fenc1", "db2inst1", "Debian-exim", "ftp", "games", "gdm", "gnats", "halt", "irc", "list", "lp", "mail", "man", "named", "news", "nobody", "ntp", "operator", "oracle8", "portage", "postfix", "postmaster", "proxy", "public", "rpc", "rwhod", "shutdown", "smmsp", "smmta", "squid", "sshd", "sync", "sys", "system", "toor", "uucp", "websphere", "www-data"]
 username = sys.argv[1]
 host = sys.argv[2]
 password_file = sys.argv[3]
